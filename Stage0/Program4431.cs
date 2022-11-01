1f7aee433ed9f480708e0e13924a2260ba97cbff
Shira1984 <shiratz325@gmail.com>
2022-11-01 16:15:30 +0200

    Initial code + Welcome functions

---
+++ Stage0/Program4431.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome4431();
+            Welcome6510();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome6510();
+
+        private static void Welcome4431()
+        {
+            Console.Write("Enter your name: ");
+            string user = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", user);
+        }
+    }
+}
