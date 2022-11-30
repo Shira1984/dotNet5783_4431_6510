77bb0f3b2ce6c59a0002879b96635e68e61b2d6b
Shira1984 <shiratz325@gmail.com>
2022-11-30 17:51:07 +0200

    Bl

---
+++ BL/BlImplementation/Cart.cs
@@ -0,0 +1,13 @@
+﻿using BlApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlImplementation;
+
+internal class Cart : ICart
+{
+    DalApi.IDal dal = new Dal.DalList();
+}
