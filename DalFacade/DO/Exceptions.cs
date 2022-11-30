c7edb81ff633c127873fd5cfe4896c3ffdc7fc21
Gili1984 <gilizilber6@gmail.com>
2022-11-30 15:46:16 +0200

    ggg

--- DalFacade/DO/Exceptions.cs
+++ DalFacade/DO/Exceptions.cs
@@ -6,5 +6,13 @@ using System.Threading.Tasks;
 
 namespace DO
 {
+    public  class NoFind:Exception
+    {
+      public NoFind(string? message) : base(message) { }
+    }
+    public class AlredyFind : Exception
+    {
+        public AlredyFind(string? message) : base(message) { }
+    }
 
 }
