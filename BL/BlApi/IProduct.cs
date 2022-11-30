bbbf2333ceab2d497b26e497d5526e8aa731524d
Shira1984 <shiratz325@gmail.com>
2022-11-30 17:35:37 +0200

    pro inter

--- BL/BlApi/IProduct.cs
+++ BL/BlApi/IProduct.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,4 +9,12 @@ namespace BlApi;
 
 public interface IProduct
 {
+    ProductItem GetProductC(int id, Cart c); // for client
+    //IEnumerable<ProductItem?> GetProductC(); // for client
+    IEnumerable<ProductForList?> GetListedProducts();
+    Product GetProductM(int id); //for manager
+    //IEnumerable<Product?> GetProductM(int id);
+    void AddProductM(Product p);
+    void DeleteProductM(int id);
+    void UpdateProductM(Product p);
 }
