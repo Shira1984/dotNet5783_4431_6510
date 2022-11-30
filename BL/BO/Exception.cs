a359919f889406e38f3a52ed76b03444039b2225
Gili1984 <gilizilber6@gmail.com>
2022-11-30 16:49:28 +0200

    BOException

--- BL/BO/Exception.cs
+++ BL/BO/Exception.cs
@@ -1,12 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BO
 {
-    internal class Exception
-    {
+   
+    
+        [Serializable]
+        public class NagtiveException : Exception
+        {
+           public NagtiveException() : base() { }
+           public NagtiveException(string? message) : base(message) { }
+           public NagtiveException(string message, Exception inner) : base(message, inner) { }
+           protected NagtiveException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
     }
+
+        [Serializable]
+        public class NotWriteException : Exception
+        {
+           public NotWriteException() : base() { }
+           public NotWriteException(string? message) : base(message) { }
+           public NotWriteException(string message, Exception inner) : base(message, inner) { }
+           protected NotWriteException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+
+        [Serializable]
+        public class NoFindException : Exception
+        {
+          public NoFindException() : base() { }
+          public NoFindException(string? message) : base(message) { }
+          public NoFindException(string message, Exception inner) : base(message, inner) { }
+          protected NoFindException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+
+        [Serializable]
+        public class AlredyFindException : Exception
+        {
+            public AlredyFindException() : base() { }
+            public AlredyFindException(string? message) : base(message) { }
+            public AlredyFindException(string message, Exception inner) : base(message, inner) { }
+            protected AlredyFindException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+    
 }
