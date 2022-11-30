5ac8407ad48b49add11ab153f22e1be8ce6b50d1
Gili1984 <gilizilber6@gmail.com>
2022-11-30 16:00:15 +0200

    not basic BO emptities

---
+++ BL/BO/ProductItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BO.Enums;
+
+
+namespace BO
+{
+    internal class ProductItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+        public int InStock { get; set; }
+
+        public bool Amount { get; set; }
+    }
+}
