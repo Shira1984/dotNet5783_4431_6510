051b577d4fa6059369394c532fd238e0a48ad976
Shira1984 <shiratz325@gmail.com>
2022-12-02 12:58:59 +0200

    main kampf

--- BlTest/Program.cs
+++ BlTest/Program.cs
@@ -21,7 +21,7 @@ namespace BlTest
             IBl bl = new Bl();
             MenuOptions mo;
 
-            IEnumerable<ProductForList?> productFL = bl.Product.GetListedProducts();
+           // IEnumerable<ProductForList?> productFL = bl.Product.GetListedProducts();
 
             Console.WriteLine(@"Welcome!
 Enter
@@ -33,6 +33,7 @@ Enter
             int opt;
             bool b = int.TryParse(Console.ReadLine(), out opt);
             mo = (MenuOptions)opt;
+           // bool flag = true;
             while (mo != 0)
             {
                 switch (mo)
@@ -41,22 +42,152 @@ Enter
                         break;
                     case MenuOptions.PRODOCT:
                         {
+                            ProductMenu pm;
+                            Console.WriteLine(@"Please press:
+0 for menu options
+1 for add
+2 for update
+3 for delete
+4 for get products list
+5 for get product (Manager option)
+6 for get product (Client option)");
+                            int optP;
+                            b = int.TryParse(Console.ReadLine(), out optP);
+                            pm = (ProductMenu)optP;
+                            switch(pm)
+                            {
+                                case ProductMenu.Main_Menu:
+                                    break;
+                                case ProductMenu.Add:
+                                    {
 
+                                        break;
+                                    }
+                                case ProductMenu.Update:
+                                    {
+
+                                        break;
+                                    }
+                                case ProductMenu.Delete:
+                                    {
+
+                                        break;
+                                    }
+                                case ProductMenu.Get_products_list:
+                                    {
+
+                                        break;
+                                    }
+                                case ProductMenu.Get_product_for_manager:
+                                    {
+
+                                        break;
+                                    }
+                                case ProductMenu.Get_product_for_client:
+                                    {
+
+                                        break;
+                                    }
+                                default:
+                                    break;
+                            }
                             break;
                         }
                     case MenuOptions.ORDER:
                         {
+                            OrderMenu om;
+                            Console.WriteLine(@"
+Please press:
+0 for main menu
+1 for get orders list
+2 for get order by id (Manager option)
+3 for update delivery date (Manager option)
+4 for update ship date (Manager option)
+5 for follow order (Manager option)");
+                            int optO;
+                            b = int.TryParse(Console.ReadLine(), out optO);
+                            om = (OrderMenu)optO;
+                            switch (om)
+                            {
+                                case OrderMenu.Main_Menu:
+                                    break;
+                                case OrderMenu.Get_orders_list:
+                                    {
 
+                                        break;
+                                    }
+                                case OrderMenu.Get_order_by_id_for_manager:
+                                    {
+
+                                        break;
+                                    }
+                                case OrderMenu.Update_delivery_date_for_manager:
+                                    {
+
+                                        break;
+                                    }
+                                case OrderMenu.Update_ship_date_for_manager:
+                                    {
+
+                                        break;
+                                    }
+                                case OrderMenu.Follow_order_for_manager:
+                                    {
+
+                                        break;
+                                    }
+                                default:
+                                    break;
+                            }
                             break;
                         }
                     case MenuOptions.CART:
                         {
+                            CartMenu cm;
+                            Console.WriteLine(@"
+Please press:
+0 for main menu
+1 for add
+2 for update
+3 for order cart");
+                            int optC;
+                            b = int.TryParse(Console.ReadLine(), out optC);
+                            cm = (CartMenu)optC;
+                            switch (cm)
+                            {
+                                case CartMenu.Main_Menu:
+                                    break;
+                                case CartMenu.Add:
+                                    {
 
+                                        break;
+                                    }
+                                case CartMenu.Update:
+                                    {
+
+                                        break;
+                                    }
+                                case CartMenu.Order_cart:
+                                    {
+
+                                        break;
+                                    }
+                                default:
+                                    break;
+                            }
                             break;
                         }
                     default:
                         break;
                 }
+                Console.WriteLine(@"Welcome!
+Enter
+0 for EXIT
+1 for Product Options
+2 for Order Options
+3 for Cart Options");
+                b = int.TryParse(Console.ReadLine(), out opt);
+                mo = (MenuOptions)opt;
             }
 
         }
