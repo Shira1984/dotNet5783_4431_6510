f84f362cdadd73f7564e3b136157cfbd10bc46d3
Gili1984 <gilizilber6@gmail.com>
2023-01-06 13:05:08 +0200

    foloworder

--- PL/OrderWindowC.xaml.cs
+++ PL/OrderWindowC.xaml.cs
@@ -21,16 +21,32 @@ namespace PL
     /// </summary>
     public partial class OrderWindowC : Window
     {
+        DateTime mydate = DateTime.Today;
+        double d = 3.0;
         public OrderWindowC(BO.Cart c)
         {
             InitializeComponent();
             OrderStatusComboBox.ItemsSource = Enum.GetValues(typeof(BO.Enums.OrderStatus));
             BO.Order o = new BO.Order();
-            o.CustomerName = c.CustomerName;
-            o.CustomerAdress = c.CustomerAdress;
-            o.CustomerEmail = c.CustomerEmail;
-            o.Items = c.Items.ToList();
-            o.OrderDate = DateTime.Today;
+            o.Items = c.Items;
+            ItemsLSTBX.DataContext = o.Items.ToList();
+            IdTextBox.Text=o.ID.ToString();
+            CstNameTextBox.Text=c.CustomerName;
+            CstEmailTextBox.Text=c.CustomerEmail;
+            CstAdressTextBox.Text=c.CustomerAdress;
+            TotalPriceTextBox.Text = c.TotalPrice.ToString();
+            OrderStatusComboBox.Text = BO.Enums.OrderStatus.Ordered.ToString();
+            //ShipDateTextBox.Text= new DateTime(mydate.Year, mydate.Month, mydate.Day).AddDays(d);
+           
+            OrderDateTextBox.Text = DateTime.Today.ToString();
+            o.CustomerName=c.CustomerName;
+            o.CustomerAdress=c.CustomerAdress;
+            o.CustomerEmail=c.CustomerEmail;
+            o.OrderDate = mydate;
+            o.Status = BO.Enums.OrderStatus.Ordered;
+
+
+            o.TotalPrice = c.TotalPrice.Value;
             o.ShipDate = null;
             o.DeliveryDate = null;
         }
@@ -39,5 +55,7 @@ namespace PL
         {
 
         }
+
+       
     }
 }
